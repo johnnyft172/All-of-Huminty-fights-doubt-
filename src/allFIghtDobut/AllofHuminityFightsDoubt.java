0d60c0d4c5094910c18fc895b1399bd242a7274b
jmintegr1 <jmintegr1@gmail.com>
2018-12-19 20:53:55 -0500

    All of Us Fight Doubt

---
+++ src/allFIghtDobut/AllofHuminityFightsDoubt.java
@@ -0,0 +1,9 @@
+package allFIghtDobut;
+
+public class AllofHuminityFightsDoubt {
+    public static void main(String[] args){
+        System.out.println("Every human being fights doubt, the only difference is the wise one push past doubt with strategies and hard work " +
+                "until they succeed as a result they build authentic confidence. Which sustains them for long term growth and greater success" +
+                "Now the cycle gets bigger and bigger and now they are more geuninely confident as a result they grow and succeed even more!!!");
+    }
+}
