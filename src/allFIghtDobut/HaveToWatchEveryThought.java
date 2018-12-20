61fb1dde75aa4e69e4ff8fc5e3a4eabe5cbee613
jmintegr1 <jmintegr1@gmail.com>
2018-12-19 21:14:20 -0500

    Stand Guard Of Your Own Thought

---
+++ src/allFIghtDobut/HaveToWatchEveryThought.java
@@ -0,0 +1,9 @@
+package allFIghtDobut;
+
+public class HaveToWatchEveryThought {
+    public static void main(String[] args) {
+        System.out.println("You have to guard against every thought and fight negative one and remember they are automatic as in strong " +
+                "in human nature. Constantly plant thought of self love and empowering thoughts just like you did all the time" +
+                "with great intensity and worked hard to get your success!");
+    }
+}
