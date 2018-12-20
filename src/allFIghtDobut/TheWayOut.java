1945306f504ee32cb6582e4f773f39d63e3313e6
jmintegr1 <jmintegr1@gmail.com>
2018-12-19 21:05:42 -0500

    They Way Out

---
+++ src/allFIghtDobut/TheWayOut.java
@@ -0,0 +1,9 @@
+package allFIghtDobut;
+
+public class TheWayOut {
+    public static void main(String[] args) {
+        System.out.println("Use the Stock Hold Syndrome and push past pain until you masachastically begin to " +
+                "enjoy your pain and transmute into pleasure. Now visualize, work hard, but you must suffer " +
+                "till you enjoy it more");
+    }
+}
